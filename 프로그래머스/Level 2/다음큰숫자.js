ea72f0124a523f3510b63deaddea6f484267c932
Ji Subin <56349582+zzzzsb@users.noreply.github.com>
2022-02-03 23:55:12 +0900

    Update 다음큰숫자.js

--- 프로그래머스/Level 2/다음큰숫자.js
+++ 프로그래머스/Level 2/다음큰숫자.js
@@ -3,7 +3,7 @@
 /*
 수빈 Solution #1
 N: 다음 큰 숫자
-time: O(log2(N!/(n-1)!))
+time: O(NlogN)
 space: O(1)
 */
 function solution(n) {
@@ -27,7 +27,7 @@ function convertBinary(n){
 
 /*
 수빈 Solution #2
-time: O()
+time: O(NlogN)
 space: O(1)
 */
 function solution(n) {
@@ -41,7 +41,7 @@ function solution(n) {
 /*
 홍빈
 하나씩 키우면서 2진수로 바꾸고 거기서 1의갯수를 세서 비교
-time O(?)
+time O(NlogN)
 space O(1)
 */
 function solution(n) {
