f35db7efca906fbcaeb830006bb675922c34098e
HongBeen <redbean-2@naver.com>
2022-02-25 01:02:05 +0900

    소수찾기 time complexity 수정

--- 프로그래머스/Level 2/후보키.js
+++ 프로그래머스/Level 2/후보키.js
@@ -61,7 +61,7 @@ function getComb(x) {
 		}
 	};
 
-	dfs(["0", "0", "0", "0"], arr);
+	dfs([], arr);
 
 	return comb;
 }
@@ -70,68 +70,70 @@ function getComb(x) {
 수빈
 */
 function solution(relation) {
-  //인덱스 조합
-  const subset=[]; 
-  //후보키 조합 
-  const set=new Set();
-  
-  function comb(cnt,max,start){
-      //내가 원하는 개수 조합 만들어졌으면
-      if(cnt===max){
-          // 최소성 확인 (인덱스 중복 검사)
-          let isMin=true;
-          let sArr=Array.from(set);
+	//인덱스 조합
+	const subset = [];
+	//후보키 조합
+	const set = new Set();
 
-          for(let i=0;i<sArr.length;i++){
-              let isContain=true;
-              //현재 조합이 후보키에 포함되는지 검사
-              for(let j=0;j<sArr[i].length;j++){
-                  if(subset.indexOf(sArr[i][j])===-1){
-                      isContain=false;
-                      break;
-                  }
-              }
-              if(isContain){
-                  isMin=false;
-                  break;
-              }
-          }
-          if(isMin){
-              //유일성 확인
-              const dict={};
-              let isOnly=true;
-              for(let i=0;i<relation.length;i++){
-                  //인덱스 조합에 해당하는 속성들 가져오기
-                  let temp=relation[i].filter((elem,idx)=>{
-                      return subset.indexOf(idx)!==-1;
-                  }).join("");
-                  if(!dict[temp]){
-                      dict[temp]=1;
-                  }
-                  //겹치면
-                  else{
-                      isOnly=false;
-                      break;
-                  }
-              }
-              if(isOnly){
-                  set.add([...subset]);
-              }
-          }
-          
-          return;
-      }
-      
-      for(let i=start;i<relation[0].length;i++){
-          subset.push(i);
-          comb(cnt+1,max,i+1);
-          subset.pop();
-      }
-  }
-  
-  for(let i=1;i<=relation[0].length;i++){
-      comb(0,i,0);
-  }
-  
-  return set.size;
+	function comb(cnt, max, start) {
+		//내가 원하는 개수 조합 만들어졌으면
+		if (cnt === max) {
+			// 최소성 확인 (인덱스 중복 검사)
+			let isMin = true;
+			let sArr = Array.from(set);
+
+			for (let i = 0; i < sArr.length; i++) {
+				let isContain = true;
+				//현재 조합이 후보키에 포함되는지 검사
+				for (let j = 0; j < sArr[i].length; j++) {
+					if (subset.indexOf(sArr[i][j]) === -1) {
+						isContain = false;
+						break;
+					}
+				}
+				if (isContain) {
+					isMin = false;
+					break;
+				}
+			}
+			if (isMin) {
+				//유일성 확인
+				const dict = {};
+				let isOnly = true;
+				for (let i = 0; i < relation.length; i++) {
+					//인덱스 조합에 해당하는 속성들 가져오기
+					let temp = relation[i]
+						.filter((elem, idx) => {
+							return subset.indexOf(idx) !== -1;
+						})
+						.join("");
+					if (!dict[temp]) {
+						dict[temp] = 1;
+					}
+					//겹치면
+					else {
+						isOnly = false;
+						break;
+					}
+				}
+				if (isOnly) {
+					set.add([...subset]);
+				}
+			}
+
+			return;
+		}
+
+		for (let i = start; i < relation[0].length; i++) {
+			subset.push(i);
+			comb(cnt + 1, max, i + 1);
+			subset.pop();
+		}
+	}
+
+	for (let i = 1; i <= relation[0].length; i++) {
+		comb(0, i, 0);
+	}
+
+	return set.size;
 }
